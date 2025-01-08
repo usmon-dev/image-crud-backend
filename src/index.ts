b4c59ef0cebf1f0bda7f9c8aa56b3a317242408f
Usmon Reyimberganov <usmonw@icloud.com>
2025-01-08 18:49:04 +0500

    connected to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { apiKeyMiddleware } from "./middlewares/apiKeyMiddleware";
+import imageRouter from "./routers/image.routes";
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,11 +12,11 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(apiKeyMiddleware)
+app.use(apiKeyMiddleware);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use("/images", )
+app.use("/images", imageRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
