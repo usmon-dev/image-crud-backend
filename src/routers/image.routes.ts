e5df59fce46330ffc1d6f7ee89f6ab68da318eb4
Usmon Reyimberganov <usmonw@icloud.com>
2025-01-08 18:46:54 +0500

    image.routes node

---
+++ src/routers/image.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  addImage,
+  deleteImage,
+  getAllImages,
+  getImageById,
+  updateImage,
+} from "../controllers/image.controller";
+
+const router = Router();
+
+router.get("/", getAllImages);
+router.get("/:id", getImageById);
+router.post("/", addImage);
+router.put("/:id", updateImage);
+router.delete("/:id", deleteImage);
